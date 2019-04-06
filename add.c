b1d523c402181cd81c8dcf177dd95d1688239a61
Rishabshirur <49330947+Rishabshirur@users.noreply.github.com>
2019-04-06 12:03:35 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void get_input(int*a,int*b)
+{
+    printf("enter the first number");
+    scanf("%d",a);
+    printf("enter the second number");
+    scanf("%d",b);
+
+}
+
+int add2nos(int a,int b)
+{
+    int c;
+    c=a+b;
+    return c;
+}
+
+void get_output(int a,int b, int c)
+{
+    printf("%d=%d+%d")
+}
+
+int main()
+{
+    
+}
